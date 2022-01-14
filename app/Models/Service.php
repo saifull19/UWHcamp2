3f3ed2d49953aad2af144823dc4f739361dda191
saifull19 <saifullanam19@gmail.com>
2022-01-14 06:07:35 -0800

    eloquent service to category

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -55,4 +55,9 @@ class Service extends Model
     {
         return $this->hasMany('App\Models\Order', 'service_id');
     }
+    
+    public function category()
+    {
+        return $this->belongsTo('App\Models\Category', 'category_id', 'id');
+    }
 }
