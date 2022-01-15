14a950d753f9df17724fffab609c1d2f345abf74
saifull19 <saifullanam19@gmail.com>
2022-01-15 08:57:06 -0800

    update index profile

--- resources/views/pages/dashboard/profile/index.blade.php
+++ resources/views/pages/dashboard/profile/index.blade.php
@@ -22,7 +22,7 @@
             <!--Main Col-->
             <div
                 id="profile"
-                class="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0"
+                class="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-50 mx-6 lg:mx-0"
             >
                 <div class="p-4 md:p-12 text-center lg:text-left">
                     <!-- Image for mobile view-->
