9fcfd1d1550cb33865e3a85bbdbf73a625970fba
saifull19 <saifullanam19@gmail.com>
2022-01-30 00:03:27 -0800

    middleware

---
+++ app/Http/Middleware/IsMember.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class isMember
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        // membuat middleware sendiri
+        if(!auth()->check() || auth()->user()->user_role_id !== 4) {
+            abort(403);
+        }
+
+        // if(!auth()->check() || !auth()->user()->user_role_id) {
+        //     abort(403);
+        // }
+
+        return $next($request);
+    }
+}
